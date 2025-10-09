a631961c98f8602a771dbadeec5efe82016268f2
JoaquinRodriguez07 <rodriguezjoaquinmariano@gmail.com>
2025-10-09 20:31:41 -0300

    agregue los repo de cada entity

---
+++ backend/kyubit-web/src/main/java/um/edu/pizzum/burgum/repository/AddressRepository.java
@@ -0,0 +1,8 @@
+package um.edu.pizzum.burgum.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import um.edu.pizzum.burgum.entities.Address;
+
+public interface AddressRepository extends JpaRepository<Address,Long> {
+
+}
---
+++ backend/kyubit-web/src/main/java/um/edu/pizzum/burgum/repository/CreationRepository.java
@@ -0,0 +1,7 @@
+package um.edu.pizzum.burgum.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import um.edu.pizzum.burgum.entities.Creation;
+
+public interface CreationRepository extends JpaRepository<Creation,Long> {
+}
