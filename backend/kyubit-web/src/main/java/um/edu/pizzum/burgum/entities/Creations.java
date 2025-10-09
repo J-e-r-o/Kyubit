268becd1317adb16ce6def5cb366cfa32ec8da00
JoaquinRodriguez07 <rodriguezjoaquinmariano@gmail.com>
2025-10-09 18:28:31 -0300

    entidades que faltaban

--- backend/kyubit-web/src/main/java/um/edu/pizzum/burgum/entities/Creations.java
+++ backend/kyubit-web/src/main/java/um/edu/pizzum/burgum/entities/Creations.java
@@ -1,4 +1,36 @@
 package um.edu.pizzum.burgum.entities;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+
 public class Creations {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private boolean isCeliac;
+
+    @Column(nullable = false)
+    private int quantity;
+
+
+    private String name;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "product_id", nullable = false)
+    private List<Products> ingredients;
+
+
 }
